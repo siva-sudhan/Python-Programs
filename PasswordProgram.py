5217ed65310c2cd7b65216fb758cbcab3b62d896
SivaSudhan <sivasudhan@SivaSudhans-MacBook-Pro.local>
2021-04-15 19:46:22 +0530

    Password checker python program added

---
+++ PasswordProgram.py
@@ -0,0 +1,17 @@
+##Password check program
+##original password of the user
+OriginalPassword = "topsecret"
+x = -1#loop exit condition
+
+#setting the number of attempts equal to three
+for i in range(3,0,x):
+    InputPassword = str(input("Enter the password : "))
+    #if the entered password is correct login the user and exit the loop
+    if(OriginalPassword == InputPassword):
+        print("You have been logged in successfully!")
+        x = -3##loop exit condition
+    #if the entered password ask the user to try again for 3 attempts
+    else:
+        j = i-1
+        print("Oops " +str(j) + " attempts left!!")
+       
